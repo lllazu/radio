0ac5ca74494dffb76cc4dfe77edefb0d0a26d468
lllazu <lazuran@abv.bg>
2016-07-26 10:36:20 +0300

    Create concat_mp4.sh

---
+++ concat_mp4.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+
+#mkfifo temp0 temp1
+#avconv -i MVI_3395.MP4 -c copy -bsf:v h264_mp4toannexb -f mpegts -y temp0 2> /dev/null & \
+#avconv -i MVI_3394.MP4 -c copy -bsf:v h264_mp4toannexb -f mpegts -y temp1 2> /dev/null & \
+#avconv -f mpegts -i "concat:temp0|temp1" -c copy -bsf:a aac_adtstoasc output.mp4
+
+
+
+#
+# . ./concat_mp4.sh MVI_3395.MP4 MVI_3394.MP4
+#
+
+
+output="output.mp4"
+
+find . -name "temp*" -exec rm {} \;
+rm -rf $output
+
+
+i=0
+cmd=""
+concat=""
+for var in "$@"
+do
+    i=$((i+1))
+    
+    mkfifo "temp$i"
+    cmd="$cmd avconv -i \"$var\" -c copy -bsf:v h264_mp4toannexb -f mpegts -y temp$i 2> /dev/null &"
+    
+    if [ $i == 1 ]; then
+        concat="concat:temp$i"
+    else
+        concat="$concat|temp$i"
+    fi
+done
+
+cmd="$cmd avconv -f mpegts -i \"$concat\" -c copy -bsf:a aac_adtstoasc \"$output\""
+echo $cmd
+eval $cmd
+
+
+find . -name "temp*" -exec rm {} \;
