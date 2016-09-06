52f216b1a5e8e658aa38619bac7e94faef827863
lllazu <lazuran@abv.bg>
2016-09-07 02:25:49 +0300

    Create mp3.sh

---
+++ mp3.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# credits
+#
+# https://github.com/rg3/youtube-dl/
+
+# install
+#
+# sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+# sudo chmod a+rx /usr/local/bin/youtube-dl
+
+# examples
+#
+# youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 https://www.youtube.com/watch?v=123
+# youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 https://soundcloud.com/xyz/123
+
+
+if [ -z "$1" ]; then
+    echo "[ERROR] url missing"
+else
+	youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 $1
+fi
