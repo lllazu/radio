8eff23cf7f188f0a93ce33d32c0e611b3584880b
laz <lllazu@github.com>
2016-10-19 14:05:45 +0300

    url to mp3

---
+++ radio-mp3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# -i http://ubuntu.hbr1.com:19800/ambient.ogg
+#	input stream
+
+# -map_metadata 0:s:0
+#	copy metadata from the first stream of the input file to global metadata of the output file
+
+# -acodec libmp3lame
+#	mp3 encoder, requires ubuntu-restricted-extras
+
+avconv -i $1 -acodec libmp3lame -map_metadata 0:s:0 "rec-$(date +%Y%m%d-%H%M%S).mp3"
