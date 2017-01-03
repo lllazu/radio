058b37cfdce914bb657ec5277117e261f3012e38
lllazu <lazuran@abv.bg>
2017-01-04 00:26:00 +0200

    Update crop-mp3.sh

--- crop-mp3.sh
+++ crop-mp3.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
-avconv -i $1 -ab 256k -ss $3 -t $4 $2
+
+# crop mp3 file
+#
+#   $1 input mp3 file
+#   $2 start cutting from
+#   $3 stop cutting to
+
+
+input_temp="/tmp/tmp-$(date +%Y%m%d-%H%M%S).mp3"
+cp "$1" $input_temp
+output_temp="/tmp/tmp-$(date +%Y%m%d-%H%M%S)-2.mp3"
+
+avconv -i $input_temp -c copy -ss $2 -to $3 $output_temp
+
+rm -f $input_temp
+cp $output_temp "${1%.*}-2.mp3"
+rm -f $output_temp
