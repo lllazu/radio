5d7e2a50216a1a176a64bc5676b7963f95bf0dde
user <lllazu@github.com>
2017-08-06 13:59:38 +0300

    avconv -to parametr

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ online radio, recording and video processing
 # rsync
 rsync -avh source/ dest/ --delete-after
 
+# mp3info
+mp3info -r a -p "%r %f \\n" *.mp3
+
 # dependencies
 ```
+sudo apt-get install libav-tools
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/bin/youtube-dl
 sudo chmod a+rx /usr/bin/youtube-dl
 sudo apt-get install mp3info
--- crop-mp3.sh
+++ crop-mp3.sh
@@ -7,6 +7,9 @@
 #   $2 start cutting from
 #   $3 stop cutting to
 
+# example 
+#    crop-mp3.sh song.mp3 00:00:00.0 00:00:00.0
+
 
 raw_bitrate=$(mp3info -r a -p "%r" "$1")
 bitrate="${raw_bitrate%.*}k"
@@ -15,7 +18,13 @@ input_temp="/tmp/tmp-$(date +%Y%m%d-%H%M%S).mp3"
 cp "$1" $input_temp
 output_temp="/tmp/tmp-$(date +%Y%m%d-%H%M%S)-2.mp3"
 
-avconv -i $input_temp -ab $bitrate -ss $2 -to $3 $output_temp
+# if avconv does not suprort -to parameter
+duration_nanosec=$(($(date -d "$3" "+%s%N") - $(date -d "$2" "+%s%N")))
+duration=$(echo "scale=2; $duration_nanosec/1000000000" | bc)
+avconv -i $input_temp -ab $bitrate -ss $2 -t $duration $output_temp
+
+# if avconv suprorts -to parameter
+#avconv -i $input_temp -ab $bitrate -ss $2 -to $3 $output_temp
 
 rm -f $input_temp
 cp $output_temp "${1%.*}-2.mp3"
