a8b7e0ca618399c3a2375fdb6a26dc6486cbf88d
laz <lllazu@github.com>
2019-01-31 11:35:20 +0200

    metadate

--- README.md
+++ README.md
@@ -19,6 +19,11 @@ id3convert -s ./*/*
 exiftool -AllDates='2018:09:15 11:11:11' -overwrite_original IMG_20180915_111111.jpg
 exiv2 IMG_20180915_111111.jpg
 
+# mp4 metadata
+exiftool -AllDates='2018:12:31 11:14:20' -overwrite_original VID_20181231_111420.mp4
+exiftool VID_20181231_111420.mp4
+mediainfo VID_20181231_111420.mp4
+
 # flv to mp4
 avconv -i input.flv -c:a copy output.mp4
 
@@ -31,6 +36,8 @@ sudo apt-get install libav-tools
 sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/bin/youtube-dl
 sudo chmod a+rx /usr/bin/youtube-dl
 sudo apt-get install mp3info
+sudo apt install mediainfo
+sudo apt-get install libimage-exiftool-perl
 sudo apt-get install libid3-tools
 sudo add-apt-repository ppa:jonathonf/ffmpeg-3
 sudo apt update && sudo apt install ffmpeg libav-tools x264 x265
---
+++ metadate.lua
@@ -0,0 +1,104 @@
+--[[
+documentation: change modification time of pictures/videos
+usage: lua metadate.lua <dir>
+usage: lua metadate.lua /media/laz/lg/snimki/2018___pixel2/E/
+dependencies: https://keplerproject.github.io/luafilesystem/
+]]
+
+local lfs = require "lfs"
+
+
+-- format: IMG_20190130_112233.jpg
+local function isMedia (fileName)
+	words = {}
+	for word in fileName:gmatch("%w+") do 
+		words[#words+1]=word
+	end
+	
+	if 
+		#words == 4 and 
+		words[2]:len() == 8 and
+		words[3]:len() == 6 and
+		(
+		words[4]:lower() == "jpg" or
+		words[4]:lower() == "png" or
+		words[4]:lower() == "mp4"
+		)
+	then
+		local media ={}
+		media.year = words[2]:sub(1,4)
+		media.month = words[2]:sub(5,6)
+		media.date = words[2]:sub(7,8)
+		media.hour = words[3]:sub(1,2)
+		media.minute = words[3]:sub(3,4)
+		media.second = words[3]:sub(5,6)
+		return media
+	end -- check
+end -- isMedia
+
+
+local function processMedia (fileName, fileNamePath)
+	local media = isMedia(fileName, fileNamePath)
+	if media then 
+		print(string.format("exiftool -AllDates='%s:%s:%s %s:%s:%s' -overwrite_original %s", 
+			media.year, media.month, media.date,
+			media.hour, media.minute, media.second,
+			fileNamePath))
+	end
+end
+
+
+local counter = 0
+local traverse
+traverse = function (file, func)
+	local from, fromObject
+	if not file then 
+		return
+	elseif type(file) == "string" then 
+		from = file
+	elseif type(file) == "table" then 
+		from = file.path
+		fromObject = file
+	end
+	
+	local attrFrom, errFrom = lfs.attributes(from)
+	if not attrFrom then return end
+	
+	if attrFrom.mode == "directory" then
+		print("# traverse: " .. from)
+		
+		local paths = {}
+		for entry in lfs.dir(from) do
+			if entry ~= "." and entry ~= ".." then
+				local pathFrom, pathTo
+				if from ~= "/" then 
+					if from:sub(#from,#from) == "/" then
+						pathFrom = from .. entry
+					else
+						pathFrom = from .. "/" .. entry
+					end
+				else
+					pathFrom = "/" .. entry
+				end
+				paths[#paths+1] = {path = pathFrom, name = entry}
+			end
+		end
+		if #paths > 0 then
+			table.sort(paths, function(x,y) return x.path < y.path end)
+			for i=1,#paths do traverse(paths[i], func) end
+		end
+	elseif attrFrom.mode == "file" then
+		counter = counter + 1
+		print("# traverse: [" .. counter .. "] " .. from)
+		if fromObject and func then func(fromObject.name, fromObject.path) end
+	end
+end
+
+
+if #arg < 1 then
+	print("usage: lua metadate.lua <dir>")
+	return
+end
+
+traverse(arg[1], processMedia)
+print("### end ###\n")
